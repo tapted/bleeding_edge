415a3e42f429cff9c4b426067c9344f9feffece0
Patrick Laub <patrick.laub@gmail.com>
2014-01-14 18:44:15 +1100

    Simplified 'pub get' step, added make zip target.
    Now make zip will create a new directory zip/ inside pub/ which only contains
    the files necessary for inclusion in the packaged app.
    
    Also the requirement for package:browser is removed, as the necessary code is
    extracted to js/browser.js. The package:browser/dart.js part of the file has
    been modified so that .dart files are replaced in the DOM by .precompiled.dart
    files. Also the path is changed to be in js/ instead of lib/src/app.
    
    The next patch will properly removed the duplicate files and fix the build
    system so that the app/ directory will not be required.
    
    R=tapted@chromium.org
    
    Review URL: https://codereview.appspot.com/51210043

--- dart/sdk/lib/_internal/pub/app/js/main.js
+++ dart/sdk/lib/_internal/pub/app/js/main.js
@@ -2,3 +2,16 @@ chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('pub.html',
     {id: 'pub', bounds: {width: 800, height: 550}});
 });
+
+// From dart package:browser/interop.js
+
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Type for remote proxies to Dart objects with dart2js.
+// WARNING: do not call this constructor or rely on it being
+// in the global namespace, as it may be removed.
+function DartObject(o) {
+  this.o = o;
+}
